b68a58283a7a4ccbe1e4430f1a23c9c03b79b2b5
Glenn-David Daniel <d2rd@outlook.com>
2018-02-06 11:17:43 -0800

    Problem running app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,20 @@
 import React from 'react';
-
+function Movie(props) {
+  return (
+    <li>
+      <button 
+        onClick={() => {
+          props.deleteMovie(props.movie.id);
+        }}
+      >
+        Delete
+      </button>{' '}
+      {props.movie.title
+    </li>
+      }
+        }
+  )
+}
 function Movie(props) {
   return <li>{props.movie.title}</li>;
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Movie from './Movie';
 
 class MovieList extends React.Component {
+  nextId = 4;
   state = {
     movies: [
       {
@@ -25,12 +26,43 @@ class MovieList extends React.Component {
     this.setState({ message: 'new Message' });
   };
 
+  getNextId = () =>{ //iterates id
+    return this.nextId++;
+  }
+
+handleSubmit(event) {
+  event.preventDefault();
+  // console.log('input value', event.target.value);
+  const newMovie = {
+    id: this.getNextId(), 
+    title: this.state.movieTitle,
+  };
+ //const existingMoview =[...movies]; // This may be 'rest' operator instead of spread
+  
+  // research 'spread' operator and 'rest' operator
+  const newMovies = [...this.state.movies, newMovie]; //PREFERRED SYNTAX uses '...' spread operator
+  // console.log('newMovies',newMovies);
+// console.log('input value', event.target.value)
+// console.log('state value', this.state.movieTitle)
+  // this.setState({ movies: newMovies});
+   this.setState({ movieTitle: event.target.value}); 
+  }
+removeMovie = id => {
+  const newMovies = this.state.movies.filter(movie =>
+  return movie.id !== id; 
+});
+this.setState({ movies: NewMovies });
+};
   render() {
     return (
       <div className="movie-list">
-        <button onClick={this.changeMessage}>Change Message</button>
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor="title">Title: </label>
+        {<div>{this.state.msg}</div>
+        <button onClick={() => {this.setState({msg: 'it shows'})}}>Go</button>
         <h1>{this.props.heading}</h1>
-        <h2>{this.state.message}</h2>
+        <h2>{this.state.message}</h2>}
+        </form>
         <ul>
           {this.state.movies.map(movie => {
             return <Movie key={movie.id} movie={movie} />;
